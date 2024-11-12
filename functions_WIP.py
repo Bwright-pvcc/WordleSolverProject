21647ab7bf3f9be713cd8dfd71382bff42848cd4
ganderson-pvcc <ga22657@email.vccs.edu>
2024-11-12 13:51:26 -0500

    Update functions_WIP.py

--- functions_WIP.py
+++ functions_WIP.py
@@ -1,4 +1,5 @@
 import csv
+from prettytable import PrettyTable
 
 #Brings the file into the program
 with open('wordle.csv', 'r')as file:
@@ -7,5 +8,27 @@ with open('wordle.csv', 'r')as file:
     #Turns the variable into a list
     wordAnswers = list(csv_reader)
 
-for word in wordAnswers:
-    print(word)
+#Gonna need these for later
+testList = []
+possibleAnswers = []
+
+
+#Creates the Table
+lettersTable = PrettyTable()
+
+#Adds Headers to the table
+lettersTable.field_names = ["Letter","Color","Position"]
+
+#Function to add user inputs to the table that will be used for storing values for checking validity of words on the list.
+def addInputs(letter, color, position):
+    lettersTable.add_row([letter, color, position])
+
+#Placeholder inputs while I wait for BEN to finish
+userLetter = input('letter \n')
+userColor = input('color \n')
+userPosition = input('position \n')
+
+#Runs the function on the user inputs.
+addInputs(userLetter, userColor, userPosition)
+
+print(lettersTable)
